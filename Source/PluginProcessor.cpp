d7159356ce994cdb2b0f8caf77780a4fb8c70eb5
marc <marc@marc.com>
2024-04-10 12:48:47 +0300

    just some readability changes

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -29,15 +29,9 @@ Synth1AudioProcessor::Synth1AudioProcessor()
     synth.addVoice(new SynthVoice());
 }
 
-Synth1AudioProcessor::~Synth1AudioProcessor()
-{
-}
+Synth1AudioProcessor::~Synth1AudioProcessor(){}
 
-//==============================================================================
-const juce::String Synth1AudioProcessor::getName() const
-{
-    return JucePlugin_Name;
-}
+const juce::String Synth1AudioProcessor::getName() const{ return JucePlugin_Name; }
 
 bool Synth1AudioProcessor::acceptsMidi() const
 {
@@ -66,36 +60,15 @@ bool Synth1AudioProcessor::isMidiEffect() const
    #endif
 }
 
-double Synth1AudioProcessor::getTailLengthSeconds() const
-{
-    return 0.0;
-}
-
-int Synth1AudioProcessor::getNumPrograms()
-{
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
-}
-
-int Synth1AudioProcessor::getCurrentProgram()
-{
-    return 0;
-}
-
-void Synth1AudioProcessor::setCurrentProgram (int index)
-{
-}
-
-const juce::String Synth1AudioProcessor::getProgramName (int index)
-{
-    return {};
-}
-
-void Synth1AudioProcessor::changeProgramName (int index, const juce::String& newName)
-{
-}
+double Synth1AudioProcessor::getTailLengthSeconds() const{ return 0.0; }
+int Synth1AudioProcessor::getNumPrograms() { return 1; }
+int Synth1AudioProcessor::getCurrentProgram() { return 0; }
+void Synth1AudioProcessor::setCurrentProgram (int index){}
+const juce::String Synth1AudioProcessor::getProgramName (int index){ return {}; }
+void Synth1AudioProcessor::changeProgramName(int index, const juce::String& newName) {}
 
 //==============================================================================
+
 void Synth1AudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
     synth.setCurrentPlaybackSampleRate(sampleRate);
@@ -105,16 +78,9 @@ void Synth1AudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock
             voice->prepareToPlay(sampleRate, samplesPerBlock, getTotalNumOutputChannels());
         }
     }
-
-
-
 }
 
-void Synth1AudioProcessor::releaseResources()
-{
-    // When playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
-}
+void Synth1AudioProcessor::releaseResources(){}
 
 #ifndef JucePlugin_PreferredChannelConfigurations
 bool Synth1AudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
@@ -123,15 +89,10 @@ bool Synth1AudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) c
     juce::ignoreUnused (layouts);
     return true;
   #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    // Some plugin hosts, such as certain GarageBand versions, will only
-    // load plugins that support stereo bus layouts.
     if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
      && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
 
-    // This checks if the input layout matches the output layout
    #if ! JucePlugin_IsSynth
     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
         return false;
@@ -141,6 +102,8 @@ bool Synth1AudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) c
   #endif
 }
 #endif
+
+//==============================================================================
 
 void Synth1AudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
 {
@@ -188,37 +151,13 @@ void Synth1AudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce:
     synth.renderNextBlock(buffer, midiMessages, 0, buffer.getNumSamples());
 }
 
-//==============================================================================
-bool Synth1AudioProcessor::hasEditor() const
-{
-    return true; // (change this to false if you choose to not supply an editor)
-}
-
-juce::AudioProcessorEditor* Synth1AudioProcessor::createEditor()
-{
-    return new Synth1AudioProcessorEditor (*this);
-}
+bool Synth1AudioProcessor::hasEditor() const{ return true; }
+juce::AudioProcessorEditor* Synth1AudioProcessor::createEditor(){ return new Synth1AudioProcessorEditor(*this); }
+void Synth1AudioProcessor::getStateInformation (juce::MemoryBlock& destData){}
+void Synth1AudioProcessor::setStateInformation (const void* data, int sizeInBytes){}
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter(){ return new Synth1AudioProcessor(); }
 
 //==============================================================================
-void Synth1AudioProcessor::getStateInformation (juce::MemoryBlock& destData)
-{
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
-}
-
-void Synth1AudioProcessor::setStateInformation (const void* data, int sizeInBytes)
-{
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
-}
-
-//==============================================================================
-// This creates new instances of the plugin..
-juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
-{
-    return new Synth1AudioProcessor();
-}
 
 juce::AudioProcessorValueTreeState::ParameterLayout Synth1AudioProcessor::createParams() {
     std::vector<std::unique_ptr<juce::RangedAudioParameter>> params;
