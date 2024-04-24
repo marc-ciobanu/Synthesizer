54f3d2cf29014066ee8207c5242eb847248c01f1
marc <marc@marc.com>
2024-04-24 21:43:58 +0300

    Added the functions to the EqData module from previous project. Needs to be properly integrated still.

--- Source/Data/EqData.cpp
+++ Source/Data/EqData.cpp
@@ -1,24 +1,145 @@
+
 /*
   ==============================================================================
 
     EqData.cpp
-    Created: 21 Apr 2024 1:14:04pm
+    Created: 13 Apr 2024 11:37:45am
     Author:  admin
 
   ==============================================================================
 */
 
+
 #include "EqData.h"
 
-void EqData::prepareToPlay(double sampleRate, int samplesPerBlock, int numChannels)
+EqData::EqData()
+{
+}
+
+EqData::~EqData()
+{
+}
+
+void EqData::prepareToPlay(double sampleRate, int samplesPerBlock)
 {
     juce::dsp::ProcessSpec spec;
 
-    spec.sampleRate = sampleRate;
-
     spec.maximumBlockSize = samplesPerBlock;
 
-    spec.numChannels = numChannels;
+    spec.numChannels = 1;
 
-    prepare()
+    spec.sampleRate = sampleRate;
+
+    leftChain.prepare(spec);
+    rightChain.prepare(spec);
 }
+
+void EqData::process(juce::AudioBuffer<float>& buffer)
+{
+    juce::dsp::AudioBlock<float> block(buffer);
+
+    auto leftBlock = block.getSingleChannelBlock(0);
+    auto rightBlock = block.getSingleChannelBlock(1);
+
+    juce::dsp::ProcessContextReplacing<float> leftContext(leftBlock);
+    juce::dsp::ProcessContextReplacing<float> rightContext(rightBlock);
+
+    leftChain.process(leftContext);
+    rightChain.process(rightContext);
+}
+
+ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts)
+{
+    ChainSettings settings;
+
+    settings.lowCutFreq = apvts.getRawParameterValue("LOWCUTFREQ")->load();
+    settings.highCutFreq = apvts.getRawParameterValue("HIGHCUTFREQ")->load();
+    settings.peakFreq = apvts.getRawParameterValue("PEAKFREQ")->load();
+    settings.peakGainInDecibels = apvts.getRawParameterValue("PEAKGAIN")->load();
+    settings.peakQuality = apvts.getRawParameterValue("PEAKQUALITY")->load();
+    settings.lowCutSlope = static_cast<Slope>(apvts.getRawParameterValue("LOWCUTSLOPE")->load());
+    settings.highCutSlope = static_cast<Slope>(apvts.getRawParameterValue("HIGHCUTSLOPE")->load());
+
+
+    return settings;
+}
+
+void EqData::updateCoefficients(Coefficients& old, const Coefficients& replacements)
+{
+    *old = *replacements;
+}
+
+template<int Index, typename ChainType, typename CoefficientType>
+void EqData::update(ChainType& chain, const CoefficientType& coefficients)
+{
+    updateCoefficients(chain.template get<Index>().coefficients, coefficients[Index]);
+    chain.template setBypassed<Index>(false);
+}
+
+template<typename ChainType, typename CoefficientType>
+void EqData::updateCutFilter(ChainType& chain, const CoefficientType& coefficients, const Slope& slope)
+{
+    chain.template setBypassed<0>(true);
+    chain.template setBypassed<1>(true);
+    chain.template setBypassed<2>(true);
+    chain.template setBypassed<3>(true);
+
+    switch (slope)
+    {
+    case Slope_48:
+    {
+        update<3>(chain, coefficients);
+    }
+    case Slope_36:
+    {
+        update<2>(chain, coefficients);
+    }
+    case Slope_24:
+    {
+        update<1>(chain, coefficients);
+    }
+    case Slope_12:
+    {
+        update<0>(chain, coefficients);
+    }
+    }
+}
+
+void EqData::updatePeakFilter(const ChainSettings& chainSettings, double sampleRate)
+{
+    auto peakCoefficients = juce::dsp::IIR::Coefficients<float>::makePeakFilter(sampleRate, chainSettings.peakFreq, chainSettings.peakQuality, juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
+
+
+    updateCoefficients(leftChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
+    updateCoefficients(rightChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
+}
+
+void EqData::updateLowCutFilter(const ChainSettings& chainSettings, double sampleRate)
+{
+    auto lowCutCoefficients = juce::dsp::FilterDesign<float>::designIIRHighpassHighOrderButterworthMethod(chainSettings.lowCutFreq, sampleRate, 2 * (chainSettings.lowCutSlope + 1));
+    auto& leftLowCut = leftChain.get<ChainPositions::LowCut>();
+    auto& rightLowCut = rightChain.get<ChainPositions::LowCut>();
+
+    updateCutFilter(leftLowCut, lowCutCoefficients, chainSettings.lowCutSlope);
+    updateCutFilter(rightLowCut, lowCutCoefficients, chainSettings.lowCutSlope);
+}
+
+void EqData::updateHighCutFilter(const ChainSettings& chainSettings, double sampleRate)
+{
+    auto highCutCoefficients = juce::dsp::FilterDesign<float>::designIIRLowpassHighOrderButterworthMethod(chainSettings.highCutFreq, sampleRate, 2 * (chainSettings.highCutSlope + 1));
+    auto& leftHighCut = leftChain.get<ChainPositions::HighCut>();
+    auto& rightHighCut = rightChain.get<ChainPositions::HighCut>();
+
+    updateCutFilter(leftHighCut, highCutCoefficients, chainSettings.highCutSlope);
+    updateCutFilter(rightHighCut, highCutCoefficients, chainSettings.highCutSlope);
+}
+
+void EqData::updateFilters(juce::AudioProcessorValueTreeState& apvts, double sampleRate)
+{
+    auto chainSettings = getChainSettings(apvts);
+
+    updateLowCutFilter(chainSettings, sampleRate);
+    updateHighCutFilter(chainSettings, sampleRate);
+    updatePeakFilter(chainSettings, sampleRate);
+}
+
--- Source/Data/EqData.h
+++ Source/Data/EqData.h
@@ -2,7 +2,7 @@
   ==============================================================================
 
     EqData.h
-    Created: 21 Apr 2024 1:14:04pm
+    Created: 13 Apr 2024 11:37:45am
     Author:  admin
 
   ==============================================================================
@@ -12,11 +12,64 @@
 
 #include <JuceHeader.h>
 
+enum Slope
+{
+    Slope_12,
+    Slope_24,
+    Slope_36,
+    Slope_48
+};
+
+struct ChainSettings
+{
+    float peakFreq{ 0 }, peakGainInDecibels{ 0 }, peakQuality{ 0 };
+    float lowCutFreq{ 0 }, highCutFreq{ 0 };
+    Slope lowCutSlope{ Slope::Slope_12 }, highCutSlope{ Slope::Slope_12 };
+};
+
+ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
+
 class EqData
 {
 public:
-    void prepareToPlay(double sampleRate, int samplesPerBlock, int numChannels);
+    EqData();
+    ~EqData();
+
+    void prepareToPlay(double sampleRate, int samplesPerBlock);
     void process(juce::AudioBuffer<float>& buffer);
-private:
+    void updateFilters(juce::AudioProcessorValueTreeState& apvts, double sampleRate);
     
+
+private:
+    using Filter = juce::dsp::IIR::Filter<float>;
+    using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
+    using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+    using Coefficients = Filter::CoefficientsPtr;
+
+    MonoChain leftChain, rightChain;
+
+    enum ChainPositions
+    {
+        LowCut,
+        Peak,
+        HighCut
+    };
+
+    void updateCoefficients(Coefficients& old, const Coefficients& replacements);
+
+    template<int Index, typename ChainType, typename CoefficientType>
+    void update(ChainType& chain, const CoefficientType& coefficients);
+
+    template<typename ChainType, typename CoefficientType>
+    void updateCutFilter(ChainType& chain, const CoefficientType& coefficients, const Slope& slope);
+
+    void updatePeakFilter(const ChainSettings& chainSettings, double sampleRate);
+    void updateLowCutFilter(const ChainSettings& chainSettings, double sampleRate);
+    void updateHighCutFilter(const ChainSettings& chainSettings, double sampleRate);
+    
+
+
+    
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(EqData)
 };
